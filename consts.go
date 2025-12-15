ffa0b65844f5730cd175b0c47444bf960f4af6de
Nathan <92413483+n8zone@users.noreply.github.com>
2025-12-15 11:03:19 -0500

    refactor: remove magic number '512' from MAX_BLOCK_SIZE

--- consts.go
+++ consts.go
@@ -4,7 +4,7 @@ const (
 	BLOCK_SIZE = 512
 	// [2 bytes] [2 bytes] [0-512 bytes]
 	// [OP CODE] [BLOCK #] [...Data....]
-	MAX_PACKET_SIZE = 516
+	MAX_PACKET_SIZE = 4 + BLOCK_SIZE
 	// Read Request
 	OPCODE_RRQ uint16 = iota
 	// Write Request
