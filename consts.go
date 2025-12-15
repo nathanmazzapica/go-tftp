41fa157f5f763f341e4bc828978f19408d9ec477
Nathan <92413483+n8zone@users.noreply.github.com>
2025-12-15 11:00:08 -0500

    feat: add MAX_BLOCK_SIZE to consts.go
                        2 bytes     2 bytes      n bytes
                       ----------------------------------
                      | Opcode |   Block #  |   Data     |
                       ----------------------------------
    
    Where 0 < n <= BLOCK_SIZE (512 in standard TFTP implementations)

--- consts.go
+++ consts.go
@@ -2,6 +2,9 @@ package gotftp
 
 const (
 	BLOCK_SIZE = 512
+	// [2 bytes] [2 bytes] [0-512 bytes]
+	// [OP CODE] [BLOCK #] [...Data....]
+	MAX_PACKET_SIZE = 516
 	// Read Request
 	OPCODE_RRQ uint16 = iota
 	// Write Request
